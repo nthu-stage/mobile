1cc4574cc37f1411d2c8c6430be025a397fe594b
skyle0115 <skyle0115@gmail.com>
2017-06-20 00:53:57 +0800

    fb login done

--- reducers/index.js
+++ reducers/index.js
@@ -3,13 +3,15 @@ import thunkMiddleware from 'redux-thunk';
 import {loadingBarReducer} from 'react-redux-loading-bar';
 import AlertReducer from './reducer_alert';
 import {WorkshopListReducer} from './reducer_workshop';
+import {FacebookLoginReducer} from './reducer_auth';
 
 export default function getStore(nav) {
     const store = createStore(combineReducers({
         nav,
         alert: AlertReducer,
         loadingBar: loadingBarReducer,
-        workshopList: WorkshopListReducer
+        workshopList: WorkshopListReducer,
+        auth: FacebookLoginReducer,
     }), compose(applyMiddleware(thunkMiddleware)));
 
     return store;
