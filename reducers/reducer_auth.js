31d2c021019d6463a10919dec0c8138c784a2a1e
Steven <stevenlin0918@gmail.com>
2017-06-21 01:17:07 +0800

    nothing

--- reducers/reducer_auth.js
+++ reducers/reducer_auth.js
@@ -1,11 +1,11 @@
 
-// const initialState = {
-//     name: null,
-//     email: null,
-//     picture_url: null,
-//     userID: null,
-//     accessToken: null,
-// };
+const initialState = {
+    name: null,
+    email: null,
+    picture_url: null,
+    userID: null,
+    accessToken: null,
+};
 
 export function FacebookLoginReducer(state = initialState, action) {
     switch (action.type) {
